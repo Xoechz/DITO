5a7672b7a69c7c5050b950dd26a78a1e00beb39d
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2025-08-18 14:57:56 +0200

    update and try collector

--- src/demo/Demo.AppHost/Program.cs
+++ src/demo/Demo.AppHost/Program.cs
@@ -4,7 +4,12 @@ var sql = builder.AddSqlServer("sql")
     .WithLifetime(ContainerLifetime.Persistent)
     .WithDataVolume();
 
-var services = Enumerable.Range(0, 10);
+var collector = builder.AddContainer("collector", "otel/opentelemetry-collector-contrib")
+    .WithContainerRuntimeArgs("--add-host=host.docker.internal:host-gateway")
+    .WithBindMount("/home/elias/Repos/DITO/src/demo/Demo.AppHost/otel-collector-config.yaml", "/etc/otel-collector-config.yaml")
+    .WithEndpoint(14318, 4318);
+
+var services = Enumerable.Range(0, 3);
 var urls = string.Join(",", services.Select(i => "http://127.0.0.1:" + (5280 + i)));
 
 foreach (var serviceIndex in services)
@@ -15,6 +20,7 @@ foreach (var serviceIndex in services)
     var migration = builder.AddProject<Projects.Demo_MigrationService>("Migration-" + serviceIndex)
         .WithEnvironment("SERVICE_NAME", "Migration-" + serviceIndex)
         .WithEnvironment("SERVICE_INDEX", serviceIndex.ToString())
+        .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:14318")
         .WithReference(demoDb)
         .WaitFor(demoDb);
 
@@ -23,6 +29,7 @@ foreach (var serviceIndex in services)
         .WithEnvironment("SERVICE_INDEX", serviceIndex.ToString())
         .WithEnvironment("TARGET_URLS", urls)
         .WithEnvironment("CRON_EXPRESSION", "*/" + (serviceIndex + 1) + " * * * *")
+        .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:14318")
         .WithHttpEndpoint(5280 + serviceIndex)
         .WithHttpsEndpoint(7160 + serviceIndex)
         .WithReference(demoDb)
