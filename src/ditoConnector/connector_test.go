634a2e7456a3a906a60eeb632454c709f48f07ef
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2025-10-18 08:18:12 +0200

    added config validation tests

--- src/ditoConnector/connector_test.go
+++ src/ditoConnector/connector_test.go
@@ -832,6 +832,87 @@ func TestCreateDefaultConfig(t *testing.T) {
 	assert.Equal(t, time.Hour, exampleConfig.MaxCacheDuration)
 }
 
+func TestConfigValidation(t *testing.T) {
+	var cfg *Config
+
+	t.Run("valid config", func(t *testing.T) {
+		cfg = createDefaultConfig().(*Config)
+		err := cfg.Validate()
+		assert.NoError(t, err)
+	})
+
+	t.Run("invalid entity key", func(t *testing.T) {
+		cfg = createDefaultConfig().(*Config)
+		cfg.EntityKey = ""
+		err := cfg.Validate()
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid job key", func(t *testing.T) {
+		cfg = createDefaultConfig().(*Config)
+		cfg.JobKey = ""
+		err := cfg.Validate()
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid max cache duration", func(t *testing.T) {
+		cfg = createDefaultConfig().(*Config)
+		cfg.MaxCacheDuration = 0
+		err := cfg.Validate()
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid baggage job key", func(t *testing.T) {
+		cfg = createDefaultConfig().(*Config)
+		cfg.BaggageJobKey = ""
+		err := cfg.Validate()
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid sampling fraction", func(t *testing.T) {
+		cfg = createDefaultConfig().(*Config)
+		cfg.SamplingFraction = 0
+		err := cfg.Validate()
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid cache shard count", func(t *testing.T) {
+		cfg = createDefaultConfig().(*Config)
+		cfg.CacheShardCount = 0
+		err := cfg.Validate()
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid queue size", func(t *testing.T) {
+		cfg = createDefaultConfig().(*Config)
+		cfg.QueueSize = 0
+		err := cfg.Validate()
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid worker count", func(t *testing.T) {
+		cfg = createDefaultConfig().(*Config)
+		cfg.WorkerCount = 0
+		err := cfg.Validate()
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid batch size", func(t *testing.T) {
+		cfg = createDefaultConfig().(*Config)
+		cfg.BatchSize = 0
+		err := cfg.Validate()
+		assert.Error(t, err)
+	})
+
+	t.Run("invalid batch timeout", func(t *testing.T) {
+		cfg = createDefaultConfig().(*Config)
+		cfg.BatchTimeout = 0
+		err := cfg.Validate()
+		assert.Error(t, err)
+	})
+
+}
+
 func assertAllUnequal(t *testing.T, items []any) {
 	for i := range items {
 		for j := i + 1; j < len(items); j++ {
