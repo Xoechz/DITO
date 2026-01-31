f734934f6aee602e75024610879990adad5e6e15
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2026-01-31 13:48:26 +0100

    added comments for the empty connector

--- src/emptyConnector/traceConnector.go
+++ src/emptyConnector/traceConnector.go
@@ -15,10 +15,6 @@ type traceConnector struct {
 	traceConsumer consumer.Traces
 }
 
-func (t *traceConnector) Capabilities() consumer.Capabilities {
-	return consumer.Capabilities{MutatesData: false}
-}
-
 func newTraceConnector(logger *zap.Logger, config component.Config, nextConsumer consumer.Traces) (*traceConnector, error) {
 	logger.Info("Building empty trace connector")
 	cfg := config.(*Config)
@@ -30,16 +26,24 @@ func newTraceConnector(logger *zap.Logger, config component.Config, nextConsumer
 	}, nil
 }
 
+// Implements connector.Traces and consumer.Traces
+func (t *traceConnector) Capabilities() consumer.Capabilities {
+	return consumer.Capabilities{MutatesData: false}
+}
+
+// Implements connector.Traces and component.Component
 func (t *traceConnector) Start(_ context.Context, _ component.Host) error {
 	t.logger.Info("Starting empty trace connector with example setting", zap.String("example_setting", t.config.ExampleSetting))
 	return nil
 }
 
+// Implements connector.Traces and component.Component
 func (t *traceConnector) Shutdown(_ context.Context) error {
 	t.logger.Info("Shutting down empty trace connector")
 	return nil
 }
 
+// Implements connector.Traces and consumer.Traces
 func (t *traceConnector) ConsumeTraces(ctx context.Context, td ptrace.Traces) error {
 	t.logger.Info("Passing through traces", zap.Int("span_count", td.SpanCount()))
 	return t.traceConsumer.ConsumeTraces(ctx, td)
